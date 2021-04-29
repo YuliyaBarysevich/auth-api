cb3d58f12d1778f612f74fa1dadc017ed85b3d4b
Yuliya Barysevich <barysevich.yuliya@gmail.com>
2021-04-28 22:10:13 -0400

    all routes work, tested manually

---
+++ index.js
@@ -0,0 +1,18 @@
+'use strict'
+
+const server = require('./src/server');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+const PORT = process.env.PORT || 3333;
+
+const options = {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+};
+
+mongoose.connect(process.env.MONGODB_URI, options)
+
+server.start(PORT)
---
+++ src/server.js
@@ -0,0 +1,40 @@
+'use strict'
+
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+
+const errorHandler = require('./error-handlers/500.js');
+const notFound = require('./error-handlers/404.js');
+const logger = require('./middleware/logger.js')
+
+const authRoutes = require('./auth/routes.js');
+const v1Routes = require('./routes/v1.js')
+const v2Routes = require('./routes/v2.js')
+
+const app = express();
+
+app.use(cors());
+app.use(morgan('dev'));
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+
+app.use(logger)
+//Routes
+app.use(authRoutes)
+app.use('/api/v1', v1Routes)
+app.use('/api/v2', v2Routes)
+
+//Catchalls
+app.use(notFound)
+app.use(errorHandler)
+
+
+module.exports = {
+  server: app, 
+  start: port => {
+    if(!port) { throw new Error ('Missing Port') }
+    app.listen(port, () => console.log(`Listening on ${port}`))
+  }
+};
